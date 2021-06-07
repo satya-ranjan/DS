7aafc2bf0a69ea84193d6247ef3690f902f398ab
satya-ranjan <satyasahoocs@gmail.com>
2021-06-07 11:25:37 +0530

    Bubble sort algorithm

---
+++ Bubble_sort.py
@@ -0,0 +1,12 @@
+#1.starting with the first element(index=0) compare the current element with the next element
+#2.if the current element is greater then the next element of the list swap the number
+#3.if the current element is less the next element move to the next element .Repeat the step 1
+
+list1= [23,65,6,2,0]
+print(list1)#unsorted list
+for j in range(len(list1)-1):
+    for i in range(len(list1)-1):
+        if list1[i] > list1[i+1]:
+            list1[i] , list1[i+1] = list1[i+1] , list1[i]
+print(list1)
+
